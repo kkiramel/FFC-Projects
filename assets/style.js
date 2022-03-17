733d79af85f179310ba2e7a507e5f4375c71f4c6
Kyra Espartinez <ksespartinez@gmail.com>
2022-03-17 23:23:23 +0800

    Works styling and content
    Fixed the layout of works and its responsiveness. Added actual content for the works.

--- assets/style.js
+++ assets/style.js
@@ -1,19 +1,24 @@
 window.onload = function() {
-    let worksArticle = document.querySelectorAll(".works__grid article");
-    let worksNum = worksArticle.length;
-    for(let x = 0; x < worksNum; x++){
-        worksArticle[x].addEventListener("mouseover", (e)=>{
-            let showDescription = e.target.closest("article");
-            showDescription.querySelector(".works__figcaption").style.zIndex = 3;
-            showDescription.querySelector("h3").style.textDecoration = "underline";
-            showDescription.querySelector(".works_description").style.opacity = 1;
-        });
-        worksArticle[x].addEventListener("mouseout", (e)=>{
-            let hideDescription = e.target.closest("article");
-            hideDescription.querySelector(".works__figcaption").style.zIndex = 1;
-            hideDescription.querySelector("h3").style.textDecoration = "none";
-            hideDescription.querySelector(".works_description").style.opacity = 0;
-        });
-    }
+    // let worksArticle = document.querySelectorAll(".works__grid article figure");
+    // let worksNum = worksArticle.length;
+    // for(let x = 0; x < worksNum; x++){
+        
+    //     worksArticle[x].addEventListener("mouseover", (e)=>{
+            
+    //         let thisWork = e.target.closest("article");
+    //         let workFigure = thisWork.querySelector("figure").style;
+    //         let workTitle = thisWork.querySelector("h3").style;
+    //         workTitle.transform = "scale(1.2)";
+            
+            
+            
+    //     });
+    //     worksArticle[x].addEventListener("mouseout", (e)=>{
+    //         let thisWorkOut = e.target.closest("article");
+    //         let workFigureOut = thisWorkOut.querySelector("figure").style;
+    //         let workTitleOut = thisWorkOut.querySelector("h3").style;
+    //         workTitleOut.fontSize = "1.1em";
+    //     });
+    // }
         
 };
